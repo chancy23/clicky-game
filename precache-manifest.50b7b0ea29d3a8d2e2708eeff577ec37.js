e73090cc007f3a5dbeab10b2113867e7ae3aa47e
Chancy <chancyleath@hotmail.com>
2018-12-14 13:52:42 -0700

    Updates

---
+++ precache-manifest.50b7b0ea29d3a8d2e2708eeff577ec37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbe8ab015e1cafa0528899d90c113b0a",
+    "url": "/clicky-game/./static/media/dark_background.cbe8ab01.jpg"
+  },
+  {
+    "revision": "578a89b40ebebfbd2446",
+    "url": "/clicky-game/./static/js/runtime~main.578a89b4.js"
+  },
+  {
+    "revision": "71a0d59cff7b0ffd666d",
+    "url": "/clicky-game/./static/js/main.71a0d59c.chunk.js"
+  },
+  {
+    "revision": "f392fb299addaa35ca89",
+    "url": "/clicky-game/./static/js/1.f392fb29.chunk.js"
+  },
+  {
+    "revision": "71a0d59cff7b0ffd666d",
+    "url": "/clicky-game/./static/css/main.813f76bb.chunk.css"
+  },
+  {
+    "revision": "d439c155774ef9dcd6e5ac7f04450ecd",
+    "url": "/clicky-game/./index.html"
+  }
+];
